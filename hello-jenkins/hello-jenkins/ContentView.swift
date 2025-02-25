721fb302634005b9f830c5a71029adb395d4dce3
Murugnanandham <muruganandham23@gmail.com>
2025-02-25 14:48:08 +0530

    Initial commit

---
+++ hello-jenkins/hello-jenkins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-jenkins
+//
+//  Created by Muruganandham on 25/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Jenkins!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello-jenkins/hello-jenkins/hello_jenkinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_jenkinsApp.swift
+//  hello-jenkins
+//
+//  Created by Muruganandham on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct hello_jenkinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
